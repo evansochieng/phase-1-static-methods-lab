d4b9a5689d0d4901664cef57d03cba941ad53008
Evans Ochieng <evansochieng121@gmail.com>
2022-09-09 18:51:15 +0300

    Wrote capitalize and sanitize static methods

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
   //add static methods here
+
+  // capitalize
+  static capitalize(string){
+    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`
+    // OR
+    // return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  // sanitize
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, "");
+  }
+
+  // titleize
+  static titleize(string){
+    
+  }
 }
